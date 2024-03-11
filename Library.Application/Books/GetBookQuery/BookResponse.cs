85e721d0fdb56def5f0df773c592fd511c472757
mtikvica <mato.tikvica@gmail.com>
2024-03-11 22:25:40 +0100

    ✨ Paginated response added on fetching books

--- Library.Application/Books/GetBookQuery/BookResponse.cs
+++ Library.Application/Books/GetBookQuery/BookResponse.cs
@@ -4,7 +4,7 @@ public sealed class BookResponse
 {
     public Guid Id { get; init; }
     public required string Title { get; init; }
-    public required List<AuthorResponse> Authors { get; init; } = [];
+    public required List<AuthorResponse> Authors { get; set; } = [];
     public int? NumberOfPages { get; init; }
     public required string PublisherName { get; init; }
     public int? PublicationYear { get; init; }
